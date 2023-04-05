2e8d855b5b12ab10346b237abe27e56def198987
Bator B <batorbb@gmail.com>
2023-04-05 22:28:29 +0200

    feat: add routing admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminPageComponent } from './page/admin-page/admin-page.component';
 import { AdminFormComponent } from './components/admin-form/admin-form.component';
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [AdminPageComponent, AdminFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
